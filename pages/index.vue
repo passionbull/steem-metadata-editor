009eb6bbccf008636cdef5899204057b5a303a16
mktcode <kontakt@markus-kottlaender.de>
2018-07-12 12:15:46 +0200

    added comments to code

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
+    <!-- Headline with wunderfull creative logo :P -->
     <h1 class="text-center pt-5">
       { }<br>
       Steem Metadata Editor
     </h1>
+
+    <!-- logged in: -->
     <div class="container pb-5" v-if="$store.state.user">
+      <!-- user info/logout button -->
       <h5 class="lead text-center pb-1">
         logged in as <a :href="'https://steemit.com/@' + $store.state.user.account.name" target="_blank">@{{ $store.state.user.account.name }}</a>
       </h5>
       <div class="text-center pb-5"><a href="#" class="btn btn-sm btn-secondary" @click="logout()">logout <i class="fas fa-sign-out-alt"></i></a></div>
 
+      <!-- tabs to switch between posts and comments -->
       <ul class="nav nav-tabs nav-justified" id="myTab" role="tablist">
         <li class="nav-item">
           <a class="nav-link active" id="home-tab" data-toggle="tab" href="#posts" role="tab">Posts</a>
@@ -18,28 +23,38 @@
           <a class="nav-link" id="profile-tab" data-toggle="tab" href="#comments" role="tab">Comments</a>
         </li>
       </ul>
+
+      <!-- lists of posts and comments -->
       <div class="tab-content" id="myTabContent">
+        <!-- lists of posts -->
         <div class="tab-pane fade show active" id="posts" role="tabpanel">
           <ul class="list-group">
+            <!-- single item -->
             <li class="list-group-item" v-for="post in $store.state.posts" :key="post.id" :post="post">
               <div class="float-left">{{ post.title }}</div>
+              <!-- button to switch active post and json_metadata -->
               <button class="btn btn-sm btn-primary float-right" data-toggle="modal" data-target="#editorModal" @click="$store.commit('setActivePost', post)"><i class="fas fa-pen"></i></button>
             </li>
           </ul>
         </div>
+
+        <!-- lists of comments -->
         <div class="tab-pane fade" id="comments" role="tabpanel">
           <ul class="list-group">
+            <!-- single item -->
             <li class="list-group-item" v-for="comment in $store.state.comments" :key="comment.id" :comment="comment">
               <div class="float-left">
                 <small>Re: {{ comment.root_title }}</small><br>
                 <small class="text-muted" v-html="renderCommentBody(comment.body)"></small>
               </div>
+              <!-- button to switch active comment and json_metadata -->
               <button class="btn btn-sm btn-primary float-right" data-toggle="modal" data-target="#editorModal" @click="$store.commit('setActivePost', comment)"><i class="fas fa-pen"></i></button>
             </li>
           </ul>
         </div>
       </div>
 
+      <!-- modal to hold the textarea to edit the jsonMetadata -->
       <div class="modal fade" id="editorModal" tabindex="-1" role="dialog" v-if="$store.state.activePost">
         <div class="modal-dialog modal-lg" role="document">
           <div class="modal-content">
@@ -53,6 +68,7 @@
               <textarea v-model="$store.state.activeJsonMetadataString" class="border-0 d-block w-100 p-3" style="height: 350px;"></textarea>
             </div>
             <div class="modal-footer">
+              <!-- always show cancel button, show save button when metadata valid, otherwise show Invalid JSON error-->
               <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
               <button type="button" class="btn btn-primary" @click="saveMetadata" v-if="isValidJson">
                 <span v-if="saving"><i class="fas fa-spinner fa-spin"></i> Saving...</span>
@@ -64,6 +80,8 @@
         </div>
       </div>
     </div>
+
+    <!-- logged out, just show SteemConnect login button -->
     <div class="text-center py-5" v-else>
       <a :href="$steemconnect.getLoginURL()" class="btn btn-primary">Login with SteemConnect</a>
     </div>
@@ -77,17 +95,22 @@ import marked from 'marked'
 export default {
   data () {
     return {
-      saving: false
+      saving: false // loading indicator for Save Changes button
     }
   },
   async mounted () {
+    // state.user will be set, when coming from auth page
+    // but not if accessed this page directly
     if (!this.$store.state.user) {
+      // in that case we look for an access token in localStorage
       const accessToken = localStorage.getItem('access_token')
       if (accessToken) {
+        // and try to login with it (await is important, for the next if to be reliable )
         await this.$store.dispatch('login', accessToken)
       }
     }
 
+    // if logged in, fetch posts/comments and set first post as active one
     if (this.$store.state.user) {
       await this.$store.dispatch('fetchPosts')
       await this.$store.dispatch('fetchComments')
@@ -95,6 +118,7 @@ export default {
     }
   },
   computed: {
+    // helper to toggle error message an button in editor modal
     isValidJson () {
       try {
         JSON.parse(this.$store.state.activeJsonMetadataString);
@@ -108,9 +132,11 @@ export default {
     logout () {
       this.$store.dispatch('logout')
     },
+    // for comments it shows the actual body in the group-list, so let's parse the markdown with marked
     renderCommentBody (body) {
       return marked(body)
     },
+    // the essence, saving the edited jsonMetadata
     saveMetadata () {
       this.saving = true
       this.$steemconnect.comment(
@@ -125,6 +151,7 @@ export default {
           this.saving = false
           if (err) console.log(err)
           else {
+            // and update data (posts/comments) again, once metadata is update
             this.$store.dispatch('fetchPosts')
             this.$store.dispatch('fetchComments')
           }
--- store/index.js
+++ store/index.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex'
 import VueSteemConnect from 'vue-steemconnect'
 import steem from 'steem'
 
+// make steemconnect available
 Vue.use(VueSteemConnect, {
   app: 'mkt.test',
   callbackURL: 'https://steem-metadata-editor.herokuapp.com/auth'
@@ -11,11 +12,11 @@ Vue.use(VueSteemConnect, {
 const createStore = () => {
   return new Vuex.Store({
     state: {
-      user: null,
-      posts: [],
-      comments: [],
-      activePost: null,
-      activeJsonMetadataString: null
+      user: null, // logged in user, via steemconnect
+      posts: [], // user's posts
+      comments: [], // user's comments
+      activePost: null, // currently selected post/comment (set by clicking the edit button)
+      activeJsonMetadataString: null // edited post's/comment's json_metadata as an indented string
     },
     mutations: {
       login (state, user) {
@@ -32,16 +33,20 @@ const createStore = () => {
       },
       setActivePost (state, post) {
         state.activePost = post
+        // always when active post changes, also change active metadata
         state.activeJsonMetadataString = JSON.stringify(JSON.parse(post.json_metadata), null, 4)
       }
     },
     actions: {
       login ({ commit }, accessToken) {
+        // return prmoise to be able to wait for the user object to be set
         return new Promise((resolve, reject) => {
+          // set access token and try to fetch user object
           Vue.SteemConnect().setAccessToken(accessToken)
           Vue.SteemConnect().me((err, user) => {
             if (err) reject(err)
             else {
+              // save user object in store
               commit('login', user)
               resolve()
             }
@@ -49,15 +54,19 @@ const createStore = () => {
         })
       },
       logout ({ commit }) {
+        // remove access token and unset user in store
         localStorage.removeItem('access_token')
         commit('logout')
       },
       fetchPosts ({ commit, state }) {
+        // return promise to be able to wait for posts to be fetched
         return new Promise((resolve, reject) => {
           steem.api.getDiscussionsByBlog({tag: state.user.account.name, limit: 100}, (err, posts) => {
             if (err) reject(err);
             else {
+              // filter to remove resteems, we need only posts from the logged in user
               posts = posts.filter(post => post.author === state.user.account.name)
+              // save posts in store
               commit('setPosts', posts)
               resolve()
             }
@@ -65,10 +74,12 @@ const createStore = () => {
         })
       },
       fetchComments ({ commit, state }) {
+        // return promise to be able to wait for comments to be fetched
         return new Promise((resolve, reject) => {
           steem.api.getDiscussionsByComments({start_author: state.user.account.name, limit: 100}, (err, comments) => {
             if (err) reject(err);
             else {
+              // save comments in store, no filtering necessary
               commit('setComments', comments)
               resolve()
             }
